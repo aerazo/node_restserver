dc807a0ac0057b18de5438a2759b5a80319bd424
aerazo <aerazo88@gmail.com>
2020-02-27 23:23:42 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//parse app/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//parse app/json
+app.use(bodyParser.json());
+
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario')
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "el nombre es necesario"
+        });
+    } else {
+        res.json({
+            usuario: body
+        });
+    }
+
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario')
+});
+
+app.listen(process.env.PORT, () => console.log(`listen in ${process.env.PORT}`));
