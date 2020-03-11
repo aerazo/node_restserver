61d5b10a43df806bd70d788651aea9ca81d17bb3
aerazo <aerazo88@gmail.com>
2020-03-11 00:07:56 -0300

    login

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //parse app/json
 app.use(bodyParser.json());
 
-app.use(require('./routes/usuario'));
+app.use(require('./routes/index.js'));
 
 
 mongoose.connect(process.env.URL_DB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
