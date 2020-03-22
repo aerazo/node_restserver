3501ac899a1851668fe7fa2a823caa4f78ef4a16
aerazo <aerazo88@gmail.com>
2020-03-22 16:39:26 -0300

    end product

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -7,12 +7,93 @@ let Producto = require('../models/producto');
 //Obtener todos los productos
 app.get('/producto', verificaToken, (req, res) => {
 
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    Producto.find({ disponible: true })
+        .skip(desde)
+        .limit(5)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
+        .exec((err, productos) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                productos
+            });
+
+
+        });
+
 });
 
 //Obtener un producto por id
 app.get('/producto/:id', verificaToken, (req, res) => {
+    let id = req.params.id;
 
+    Producto.findById(id)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
+        .exec((err, productoDB) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            if (!productoDB) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'id not exists'
+                    }
+                });
+            }
+
+            res.json({
+                ok: true,
+                productoDB
+            })
+
+
+        });
 });
+
+
+//Buscar producto
+app.get('/producto/search/:term', verificaToken, (req, res) => {
+
+    let termino = req.params.term;
+
+    let regex = new RegExp(termino, 'i');
+
+    Producto.find({ nombre: regex })
+        .populate('categoria', 'nombre')
+        .exec((err, productosDB) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                productos: productosDB
+            });
+        });
+});
+
 
 //Crear un producto with user and category
 app.post('/producto', verificaToken, (req, res) => {
@@ -85,8 +166,46 @@ app.put('/producto/:id', verificaToken, (req, res) => {
 });
 
 //Eliminar un producto cambiar el disponible a false
-app.delete('/producto', verificaToken, (req, res) => {
+app.delete('/producto/:id', verificaToken, (req, res) => {
 
+    let id = req.params.id;
+
+    Producto.findById(id, (err, productoDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'id not exists'
+                }
+            });
+        }
+
+        productoDB.disponible = false;
+        productoDB.save((err, productoBorrado) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                producto: productoBorrado
+            });
+
+        });
+
+    });
 });
 
 module.exports = app;
